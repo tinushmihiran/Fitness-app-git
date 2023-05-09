3d57580d834076910121a1cd66c3dca9232ab767
tinush <tinushmiiran18@gmail.com>
2023-05-09 03:33:01 -0700

    Initial Commit

---
+++ Fitness app/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Fitness app
+//
+//  Created by Tinush mihiran on 2023-05-09.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
