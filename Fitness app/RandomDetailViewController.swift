f10547c88de2a01d1bac369001fa6829ffd3b185
tinush <tinushmiiran18@gmail.com>
2023-05-18 13:04:08 -0700

    random button complete with random algorithm

--- Fitness app/RandomDetailViewController.swift
+++ Fitness app/RandomDetailViewController.swift
@@ -4,7 +4,6 @@
 //
 //  Created by Tinush mihiran on 2023-05-18.
 //
-
 import UIKit
 import Firebase
 
@@ -18,8 +17,8 @@ class RandomDetailViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        
+        view.backgroundColor = .white
+        fetchRandomDocument()
         
         // Fetch the data from Firestore
         db.collection("RandomExercises").getDocuments() { (querySnapshot, error) in
@@ -32,217 +31,115 @@ class RandomDetailViewController: UIViewController {
                     newExercisess.append(exercises)
                 }
                 self.exercises = newExercisess
-                self.addCardViews()
+                //self.addCardViews()
                 print("Fetched \(self.exercises.count) Exercisess")
             }
         }
         
-        // Add background image to the view
-                let backgroundImage = UIImageView(frame: view.bounds)
-                backgroundImage.image = UIImage(named: "RoundViewOne") // Replace "your-background-image" with the actual image name
-                backgroundImage.contentMode = .scaleAspectFill
-                view.addSubview(backgroundImage)
-                view.sendSubviewToBack(backgroundImage)
     }
     
     @objc func goButtonTapped() {
         navigationController?.popViewController(animated: true)
     }
     
-    @objc func cardTapped(_ sender: UITapGestureRecognizer) {
-        guard let cardView = sender.view else { return }
-        guard let index = cardView.tag as Int? else { return }
-        let selectedexercise = exercises[index]
-        
-        let cardDetailsVC = ExercisessDetailsViewController(exercise: selectedexercise)
-        navigationController?.pushViewController(cardDetailsVC, animated: true)
-    }
-    
-    func addCardViews() {
-        let scrollView = UIScrollView(frame: view.bounds)
-        scrollView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
-        scrollView.showsVerticalScrollIndicator = true
-        scrollView.alwaysBounceVertical = true
-        
-      
-        
-        var yPosition: CGFloat = 100.0
-        let contentWidth = view.frame.width - 40.0
-        
-        // Create a title label
-           let titleLabel = UILabel(frame: CGRect(x: 20.0, y: yPosition, width: contentWidth, height: 30.0))
-           titleLabel.text = "All Random Exercises"
-           titleLabel.textColor = .white
-           titleLabel.font = UIFont.systemFont(ofSize: 34.0, weight: .bold)
-           scrollView.addSubview(titleLabel)
-           yPosition += 50.0
-        
-        // Create a "Go" button
-          let goButton = UIButton(type: .system)
-          goButton.setTitle("Go back", for: .normal)
-          goButton.setTitleColor(.white, for: .normal)
-          goButton.backgroundColor = .systemBlue
-          goButton.layer.cornerRadius = 10.0
-          goButton.frame = CGRect(x: 20.0, y: yPosition, width: contentWidth, height: 50.0)
-          goButton.addTarget(self, action: #selector(goButtonTapped), for: .touchUpInside)
-          scrollView.addSubview(goButton)
-          yPosition += 70.0
-        
-        for (index, exercises) in exercises.enumerated() {
-            let cardView = UIView(frame: CGRect(x: 20.0, y: yPosition, width: contentWidth, height: 200.0))
-            cardView.backgroundColor = .white
-            cardView.layer.cornerRadius = 10.0
-            cardView.layer.shadowOffset = CGSize(width: 0.0, height: 2.0)
-            cardView.layer.shadowRadius = 2.0
-            cardView.layer.shadowOpacity = 0.2
-            
-            let titleIcon = UIImageView(image: UIImage(systemName: "circle.fill"))
-            titleIcon.tintColor = .systemBlue
-            titleIcon.frame = CGRect(x: 20.0, y: 25.0, width: 20.0, height: 20.0)
-            cardView.addSubview(titleIcon)
-            
-            let nameLabel = UILabel(frame: CGRect(x: 50.0, y: 20.0, width: cardView.frame.width - 70.0, height: 30.0))
-            nameLabel.text = exercises.name
-            nameLabel.font = UIFont.systemFont(ofSize: 22.0, weight: .semibold)
-            cardView.addSubview(nameLabel)
-            
-            let stackView = UIStackView(frame: CGRect(x: 50.0, y: 55.0, width: cardView.frame.width - 70.0, height: 120.0))
-            stackView.axis = .vertical
-            stackView.spacing = 5.0
-            stackView.distribution = .fillEqually
-            
-            let exercisedescriptionLabel = UILabel()
-            exercisedescriptionLabel.text = "exercisedifficulty: \(exercises.level)"
-            exercisedescriptionLabel.font = UIFont.systemFont(ofSize: 16.0, weight: .medium)
-            stackView.addArrangedSubview(exercisedescriptionLabel)
-            
-            let durationLabel = UILabel()
-            durationLabel.text = "type: \(exercises.details)"
-            durationLabel.font = UIFont.systemFont(ofSize: 16.0, weight: .medium)
-            stackView.addArrangedSubview(durationLabel)
-            
-            let repetitionsLabel = UILabel()
-            repetitionsLabel.text = "muscle:\(exercises.days)"
-            repetitionsLabel.font = UIFont.systemFont(ofSize: 22.0, weight: .semibold)
-            repetitionsLabel.textAlignment = .left
-            stackView.addArrangedSubview(repetitionsLabel)
-            
-            cardView.addSubview(stackView)
-            stackView.translatesAutoresizingMaskIntoConstraints = false
-            NSLayoutConstraint.activate([
-                stackView.leadingAnchor.constraint(equalTo: nameLabel.leadingAnchor),
-                stackView.trailingAnchor.constraint(equalTo: cardView.trailingAnchor, constant: -20.0),
-                stackView.topAnchor.constraint(equalTo: nameLabel.bottomAnchor, constant: 10.0),
-                stackView.bottomAnchor.constraint(equalTo: cardView.bottomAnchor, constant: -10.0)
-            ])
-            
-            let difficultyIcon = UIImageView(image: UIImage(systemName: "bolt.fill"))
-            difficultyIcon.tintColor = .systemYellow
-            difficultyIcon.frame = CGRect(x: cardView.frame.width - 45.0, y: 25.0, width: 20.0, height: 20.0)
-            cardView.addSubview(difficultyIcon)
-            
-            // Add a tap gesture recognizer to the card view
-            let tapGesture = UITapGestureRecognizer(target: self, action: #selector(cardTapped(_:)))
-            cardView.addGestureRecognizer(tapGesture)
-            cardView.isUserInteractionEnabled = true
-            cardView.tag = index
-            
-            scrollView.addSubview(cardView)
-            
-            yPosition += 220.0
+  
+    func fetchRandomDocument() {
+        db.collection("RandomExercises").getDocuments() { (querySnapshot, error) in
+            if let error = error {
+                print("Error getting documents: \(error)")
+            } else {
+                guard let documents = querySnapshot?.documents else {
+                    print("No documents found")
+                    return
+                }
+                
+                let randomIndex = Int.random(in: 0..<documents.count)
+                let randomDocument = documents[randomIndex]
+                
+                let randomExercise = Exercisess(snapshot: randomDocument)
+                self.displayExerciseDetails(exercise: randomExercise)
+            }
         }
-        
-        // Set the content size of the scroll view
-        let contentHeight = yPosition + 20.0
-        scrollView.contentSize = CGSize(width: contentWidth, height: contentHeight)
-        
-        // Add the scroll view to the main view
-        view.addSubview(scrollView)
     }
-}
 
-
-class ExercisessDetailsViewController: UIViewController {
+    func displayExerciseDetails(exercise: Exercisess) {
+        // Clear the view
+        for subview in view.subviews {
+            subview.removeFromSuperview()
+        }
     
-    let exercise: Exercisess
-    let goBackButton = UIButton()
-    
-    init(exercise: Exercisess) {
-        self.exercise = exercise
-        super.init(nibName: nil, bundle: nil)
-    }
-    
-    required init?(coder aDecoder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        view.backgroundColor = .white
+        
         // Create and configure labels
-               let nameLabel = UILabel()
-               nameLabel.text = exercise.name
+        let nameLabel = UILabel()
+        nameLabel.text = exercise.name
         nameLabel.font = UIFont.systemFont(ofSize: 30, weight: .bold)
         nameLabel.textColor = UIColor.label
         nameLabel.numberOfLines = 0
-               let exercisedifficultyLabel = UILabel()
+        nameLabel.translatesAutoresizingMaskIntoConstraints = false
+        
+        let exercisedifficultyLabel = UILabel()
         exercisedifficultyLabel.text = exercise.level
         exercisedifficultyLabel.font = UIFont.systemFont(ofSize: 16, weight: .medium)
         exercisedifficultyLabel.textColor = UIColor.secondaryLabel
         exercisedifficultyLabel.numberOfLines = 0
-               
-               let typeLabel = UILabel()
-        typeLabel.text = "details: \(exercise.details) details"
+        exercisedifficultyLabel.translatesAutoresizingMaskIntoConstraints = false
+        
+        let typeLabel = UILabel()
+        typeLabel.text = "Details: \(exercise.details)"
         typeLabel.font = UIFont.systemFont(ofSize: 16, weight: .medium)
         typeLabel.textColor = UIColor.secondaryLabel
-               
-               let muscleLabel = UILabel()
-        muscleLabel.text = "days: \(exercise.days)"
+        typeLabel.translatesAutoresizingMaskIntoConstraints = false
+        
+        let muscleLabel = UILabel()
+        muscleLabel.text = "Days: \(exercise.days)"
         muscleLabel.font = UIFont.systemFont(ofSize: 16, weight: .medium)
         muscleLabel.textColor = UIColor.secondaryLabel
         muscleLabel.numberOfLines = 0
+        muscleLabel.translatesAutoresizingMaskIntoConstraints = false
+      
         
-               
-               // Add labels to the view
-               view.addSubview(nameLabel)
-               view.addSubview(exercisedifficultyLabel)
-               view.addSubview(typeLabel)
-               view.addSubview(muscleLabel)
-               
-
+        // Add labels to the view
+        view.addSubview(nameLabel)
+        view.addSubview(exercisedifficultyLabel)
+        view.addSubview(typeLabel)
+        view.addSubview(muscleLabel)
+        
         // Set up the labels in the view
-        let stackView = UIStackView(arrangedSubviews: [nameLabel, exercisedifficultyLabel, typeLabel, muscleLabel])
-        stackView.axis = .vertical
-        stackView.alignment = .leading
-        stackView.spacing = 16
-        stackView.translatesAutoresizingMaskIntoConstraints = false
-        
-        view.addSubview(stackView)
         NSLayoutConstraint.activate([
-            stackView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 32),
-            stackView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
-            stackView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20)
+            nameLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 32),
+            nameLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            nameLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+            
+            exercisedifficultyLabel.topAnchor.constraint(equalTo: nameLabel.bottomAnchor, constant: 16),
+            exercisedifficultyLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            exercisedifficultyLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+            
+            typeLabel.topAnchor.constraint(equalTo: exercisedifficultyLabel.bottomAnchor, constant: 16),
+            typeLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            typeLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+            
+            muscleLabel.topAnchor.constraint(equalTo: typeLabel.bottomAnchor, constant: 16),
+            muscleLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            muscleLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20)
         ])
-        
-        // Set up the "Go Back" button
-        goBackButton.setTitle("Go Back", for: .normal)
-        goBackButton.setTitleColor(UIColor.systemBlue, for: .normal)
-        goBackButton.titleLabel?.font = UIFont.systemFont(ofSize: 20, weight: .medium)
-        goBackButton.layer.cornerRadius = 8
-        goBackButton.layer.borderWidth = 2
-        goBackButton.layer.borderColor = UIColor.systemBlue.cgColor
-        goBackButton.addTarget(self, action: #selector(goBack), for: .touchUpInside)
-        goBackButton.translatesAutoresizingMaskIntoConstraints = false
-        
-        view.addSubview(goBackButton)
-        
-        NSLayoutConstraint.activate([
-            goBackButton.topAnchor.constraint(equalTo: stackView.bottomAnchor, constant: 16),
-            goBackButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-        ])
+        // Add "Go Back" button
+               let goButton = UIButton(type: .system)
+               goButton.setTitle("Go Back", for: .normal)
+               goButton.titleLabel?.font = UIFont.systemFont(ofSize: 16, weight: .medium)
+               goButton.tintColor = .white
+               goButton.backgroundColor = .systemBlue
+               goButton.layer.cornerRadius = 8
+               goButton.translatesAutoresizingMaskIntoConstraints = false
+               goButton.addTarget(self, action: #selector(goButtonTapped), for: .touchUpInside)
+               view.addSubview(goButton)
+               
+               NSLayoutConstraint.activate([
+                   goButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+                   goButton.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -32),
+                   goButton.widthAnchor.constraint(equalToConstant: 120),
+                   goButton.heightAnchor.constraint(equalToConstant: 40)
+               ])
     }
-    
-    @objc func goBack() {
-        navigationController?.popViewController(animated: true)
-    }
+
+
 }
